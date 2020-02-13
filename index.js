adbed513648272aa84508ada271df67703801c35
Chris Engel <Chris.Engel@yahoo.com>
2020-02-13 17:52:39 -0500

    typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require('./server.js');
 
-port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log('\n* Server Running on http://localhost:4000 *\n');
-});
+    console.log(`running on port: ${port}`)
+})
