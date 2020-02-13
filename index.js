6755e209b466bcf8a2513f9cf95a2f6b1f2535bd
Chris Engel <Chris.Engel@yahoo.com>
2020-02-13 17:43:49 -0500

    now it is ready for deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // code away!
 const server = require('./server.js');
 
-server.listen(4000, () => {
+port = process.env.PORT || 4000;
+server.listen(port, () => {
   console.log('\n* Server Running on http://localhost:4000 *\n');
 });
